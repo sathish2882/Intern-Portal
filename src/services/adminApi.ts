aa06f7d4048d05c6c9118484ef5fcc6d2221c9c2
MOHAN <mohan1999c@gmail.com>
2026-03-28 11:51:11 +0530

    api fetched

--- src/services/adminApi.ts
+++ src/services/adminApi.ts
@@ -13,6 +13,13 @@ export const getAllUsers = () => {
   return API.get("/get_all_users?page_no=1&page_size=10")
 }
 
+
+export const viewAttendanceByAdminApi = (userId: number | string) => {
+  return API.post('/viewbyadmin', null, {
+    params: { user_id: userId },
+  })
+}
+
 export const getBatchUsers = (batchId: number) => {
   return API.get(`/get_userby_batch/${batchId}`)
 }
@@ -20,4 +27,6 @@ export const getBatchUsers = (batchId: number) => {
 export const deleteUser = ()=>{
   return API.delete("/users/")
 }
+
+
 
