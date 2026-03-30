c2c99996944ba05c01cf973dda5792713bcd2552
sathish <six-sigma-solutions>
2026-03-30 10:43:52 +0530

    download feature updated

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,25 +1,29 @@
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
 
-export const downloadExcel = (data: any[]) => {
-  // Convert JSON to worksheet
+interface DownloadExcelOptions {
+  filename?: string;
+  sheetName?: string;
+}
+
+export const downloadExcel = (
+  data: Record<string, unknown>[],
+  options?: DownloadExcelOptions,
+) => {
+  const { filename = "users_data.xlsx", sheetName = "Sheet1" } = options ?? {};
+
   const worksheet = XLSX.utils.json_to_sheet(data);
-
-  // Create workbook
   const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet, "Users");
+  XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
 
-  // Convert to binary
   const excelBuffer = XLSX.write(workbook, {
     bookType: "xlsx",
     type: "array",
   });
 
-  // Create blob
   const fileData = new Blob([excelBuffer], {
     type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
   });
 
-  // Download file
-  saveAs(fileData, "users_data.xlsx");
+  saveAs(fileData, filename);
 };
